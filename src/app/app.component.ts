42d07460447d5950d7f2ca7ae20d2d68f34e1154
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-11-10 15:49:01 -0500

    ya vale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,21 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideIonicAngular } from '@ionic/angular/standalone';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [IonicModule]
+})
+export class AppComponent {
+  title = 'misupalogin';
+}
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
